7f9997b6f0e39113ec9448d955e356618e6eddea
Kai Horstmann <horstmannkai@hotmail.com>
2023-10-03 23:39:29 +0200

    Core/Inc/lwipopts.h: Activate LWIP debugging.
    Signed-off-by: Kai Horstmann <horstmannkai@hotmail.com>

--- Core/Inc/lwipopts.h
+++ Core/Inc/lwipopts.h
@@ -278,45 +278,47 @@
  * Debug messages
  */
 
+#define LWIP_DEBUG 1
+
 #define 	LWIP_DBG_MIN_LEVEL   LWIP_DBG_LEVEL_ALL
 
 #define 	LWIP_DBG_TYPES_ON   LWIP_DBG_OFF
 
-#define 	ETHARP_DEBUG   LWIP_DBG_OFF
+#define 	ETHARP_DEBUG   LWIP_DBG_ON
 
-#define 	NETIF_DEBUG   LWIP_DBG_OFF
+#define 	NETIF_DEBUG   LWIP_DBG_ON
 
-#define 	PBUF_DEBUG   LWIP_DBG_OFF
+#define 	PBUF_DEBUG   LWIP_DBG_ON
 
 #define 	API_LIB_DEBUG   LWIP_DBG_OFF
 
-#define 	API_MSG_DEBUG   LWIP_DBG_OFF
+#define 	API_MSG_DEBUG   LWIP_DBG_ON
 
 #define 	SOCKETS_DEBUG   LWIP_DBG_OFF
 
-#define 	ICMP_DEBUG   LWIP_DBG_OFF
+#define 	ICMP_DEBUG   LWIP_DBG_ON
 
 #define 	IGMP_DEBUG   LWIP_DBG_OFF
 
-#define 	INET_DEBUG   LWIP_DBG_OFF
+#define 	INET_DEBUG   LWIP_DBG_ON
 
-#define 	IP_DEBUG   LWIP_DBG_OFF
+#define 	IP_DEBUG   LWIP_DBG_ON
 
 #define 	IP_REASS_DEBUG   LWIP_DBG_OFF
 
 #define 	RAW_DEBUG   LWIP_DBG_OFF
 
-#define 	MEM_DEBUG   LWIP_DBG_OFF
+#define 	MEM_DEBUG   LWIP_DBG_ON
 
-#define 	MEMP_DEBUG   LWIP_DBG_OFF
+#define 	MEMP_DEBUG   LWIP_DBG_ON
 
-#define 	SYS_DEBUG   LWIP_DBG_OFF
+#define 	SYS_DEBUG   LWIP_DBG_ON
 
 #define 	TIMERS_DEBUG   LWIP_DBG_OFF
 
-#define 	TCP_DEBUG   LWIP_DBG_OFF
+#define 	TCP_DEBUG   LWIP_DBG_ON
 
-#define 	TCP_INPUT_DEBUG   LWIP_DBG_OFF
+#define 	TCP_INPUT_DEBUG   LWIP_DBG_ON
 
 #define 	TCP_FR_DEBUG   LWIP_DBG_OFF
 
@@ -326,7 +328,7 @@
 
 #define 	TCP_WND_DEBUG   LWIP_DBG_OFF
 
-#define 	TCP_OUTPUT_DEBUG   LWIP_DBG_OFF
+#define 	TCP_OUTPUT_DEBUG   LWIP_DBG_ON
 
 #define 	TCP_RST_DEBUG   LWIP_DBG_OFF
 
@@ -334,19 +336,19 @@
 
 #define 	UDP_DEBUG   LWIP_DBG_OFF
 
-#define 	TCPIP_DEBUG   LWIP_DBG_OFF
+#define 	TCPIP_DEBUG   LWIP_DBG_ON
 
 #define 	SLIP_DEBUG   LWIP_DBG_OFF
 
-#define 	DHCP_DEBUG   LWIP_DBG_OFF
+#define 	DHCP_DEBUG   LWIP_DBG_ON
 
-#define 	AUTOIP_DEBUG   LWIP_DBG_OFF
+#define 	AUTOIP_DEBUG   LWIP_DBG_ON
 
 #define 	DNS_DEBUG   LWIP_DBG_OFF
 
-#define 	IP6_DEBUG   LWIP_DBG_OFF
+#define 	IP6_DEBUG   LWIP_DBG_ON
 
-#define 	DHCP6_DEBUG   LWIP_DBG_OFF
+#define 	DHCP6_DEBUG   LWIP_DBG_ON
 
 /*
  * Performance
